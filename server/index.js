9aad7a52bdcd7a1ea2e72a9db606429b389f0557
Jagini <acjagini@amd.com>
2023-05-15 12:12:24 +0530

    a

--- server/index.js
+++ server/index.js
@@ -29,6 +29,17 @@ app.use("/general",generalRoutes)
 app.use("/management",managementRoutes)
 app.use("/sales",salesRoutes)
 
+const PORT=process.env.PORT||9000;
+
+mongoose.connect(process.env.MONGO_URL,{
+useNewUrlParser:true,
+useUnifiedTopology:true,
+}).then(()=>
+{
+ app.listen(PORT,()=>console.log(`Server started om ${PORT}`))   
+}).catch((error)=>
+console.log(error)
+)
 
 
 
