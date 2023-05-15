b063a96ca8a8bd99bec94d83acafd15daf369368
Jagini <acjagini@amd.com>
2023-05-15 14:54:45 +0530

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { useMemo } from "react";
+import { createTheme } from "@mui/material";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return <div className="App"></div>;
 }
 
