a9efc12b52649fc6e114e3b0ec821f1b5c3954f8
achyutjagini <achyut.jagini@gmail.com>
2023-05-16 21:56:16 +0530

    a

--- client/src/scenes/layout/index.js
+++ client/src/scenes/layout/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
+const { Box } = require("@mui/material")
 
 const Layout = () => {
     return (
         <div>
             <p> Layout</p>
+            {/* <Box display="flex" width="80px" justify-content="center" height="80px" background="white" z-index="1">
+            </Box>*/}
+
         </div>
     )
 }
