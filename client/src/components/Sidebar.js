749eb2c2f17adfac2be56d4d1ded84dd40cac66c
achyutjagini <achyut.jagini@gmail.com>
2023-05-19 13:26:08 +0530

    a

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -178,8 +178,10 @@ const Sidebar = ({
                 }
                 const lcText = text.toLowerCase();
 
+                {/* remove padding of listitem*/ }
                 return (
-                  <ListItem key={text} disablePadding>
+
+                  < ListItem key={text} disablePadding >
                     <ListItemButton
                       onClick={() => {
                         navigate(`/${lcText}`);
@@ -207,18 +209,62 @@ const Sidebar = ({
                       >
                         {icon}
                       </ListItemIcon>
+
+                      {/*the text like dashboard,products*/}
                       <ListItemText primary={text} />
+
+                      {/*conditional rendering of a chevronRight in case it's the current path*/}
                       {active === lcText && (
                         <ChevronRightOutlined sx={{ ml: "auto" }} />
                       )}
+
                     </ListItemButton>
                   </ListItem>
                 );
               })}
-            </List>
-            <Divider />
 
+            </List>
+            {/*list completed*/}
+            <Divider />
           </Box>
+          {/*
+          <Box position="absolute" bottom="2rem">
+            <Divider />
+            <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{
+                  color: theme.palette.secondary[300],
+                  fontSize: "25px ",
+                }}
+              />
+            </FlexBetween>    </Box>
+              */}
+
+
         </Drawer>
       )
       }
