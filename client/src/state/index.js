2296b7860fa08efae545fc53d6e377b9074802aa
achyutjagini <achyut.jagini@gmail.com>
2023-05-19 14:37:57 +0530

    a

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 /*the global state is a js object */
 const initialState = {
   mode: "dark",
+  userId: "63701cc1f03239b7f700000e",
 };
 
 //think as functions that change the global state
