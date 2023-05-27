55c40e8efe3187d528c1798e2436e0e25b1bbfff
achyutjagini <achyut.jagini@gmail.com>
2023-05-27 14:13:31 +0530

    a

--- server/index.js
+++ server/index.js
@@ -14,8 +14,9 @@ import salesRoutes from "./routes/sales.js"
 import User from './models/User.js'
 import Product from './models/Product.js'
 import ProductStat from './models/ProductStat.js'
+import Transaction from './models/Transaction.js'
 
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js"
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
 
 dotenv.config();
 const app = express();
@@ -44,6 +45,8 @@ mongoose.connect(process.env.MONGO_URL, {
     app.listen(PORT, () => console.log(`Server started on ${PORT}`))
     //Product.insertMany(dataProduct)
     // ProductStat.insertMany(dataProductStat)
+    // Transaction.insertMany(dataTransaction)
+
 
 }).catch((error) =>
     console.log(error)
